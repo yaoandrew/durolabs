2ad10f38c3d6e10a1556bc09ddfd07e14f59cac6
Andrew J Yao <yaoandrew@gmail.com>
2017-02-06 04:03:52 -0800

    Cleaned up branch

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import { Router, Route, hashHistory } from 'react-router'
 import { render } from 'react-dom'
 import App from './app'
 import PartDetailList from './partDetailList'
+
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App} />
